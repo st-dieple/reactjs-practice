4049dd2a985cff1d59ec9a945aaac4db7c56291a
dieplect <diep.le@supremetech.vn>
2020-06-21 02:11:06 +0700

    Make banner

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -23,8 +23,16 @@ function App() {
           </ul>
         </div>
       </header>
-      <div className="page-banner">
-        <img src={require(`../assets/images/h1-slide3-img.jpg`)} alt="h1-slide3-img.jpg" />
+      <div className="page-banner container">
+        <div className="page-banner-inner">
+          <div className="banner-item-left">
+            <h3 className="caption">CONTEMPORARY DESIGN. </h3>
+            <p className="des">A large set of beautiful & fully flexible homepage layouts <br/> lets you create your website quickly & easily. </p>
+          </div>
+          <div className="banner-item-right">
+            <img src={require(`../assets/images/h1-slide3-img.jpg`)} alt="h1-slide3-img.jpg" />
+          </div>
+        </div>
       </div>
       <div className="page-wrapper"></div>
       <footer>
