14583801dc172855f11eaf59b3b814b818bf04b5
dieplect <diep.le@supremetech.vn>
2020-06-21 19:05:09 +0700

    List rendering

--- src/app/components/features/product-item.tsx
+++ src/app/components/features/product-item.tsx
@@ -1,1 +1,17 @@
 import React from 'react';
+
+function ProductItem(props: any) {
+  return (
+    <div className="product">
+      <div className="product-img">
+        <img src={require(`../../../assets/images/${props.image}`)} alt={props.image}/>
+      </div>
+      <div className="product-info">
+        <p className="product-name">{props.name}</p>
+        <p className="product-price">${props.price}</p>
+      </div>
+    </div>
+  );
+}
+
+export default ProductItem;
--- src/app/components/features/product-list.tsx
+++ src/app/components/features/product-list.tsx
@@ -1,100 +1,23 @@
 import React from 'react';
+import ProductItem from './product-item';
 
-function ProductList() {
+function ProductList(props: any) {
+  console.log('list', props, props.products);
+  const products = props.products
+  let list = products.map((item: any) => 
+    <li className="product-list-item" key={item.id.toString()}>
+      <ProductItem name={item.name}
+                    price={item.price}
+                    image={item.image} />
+    </li>
+  );
   return (
     <div className="product-list container">
       <ul className="product-list-inner">
-        <li className="product-list-item">
-          <div className="product">
-            <div className="product-img">
-              <img src={require('../../../assets/images/h1-product-1.jpg')} alt="h1-product-1.jpg"/>
-            </div>
-            <div className="product-info">
-              <p className="product-name">Basket With Handles</p>
-              <p className="product-price">$160</p>
-            </div>
-          </div>
-        </li>
-        <li className="product-list-item">
-          <div className="product">
-            <div className="product-img">
-              <img src={require('../../../assets/images/h1-product-2.jpg')} alt="h1-product-1.jpg"/>
-            </div>
-            <div className="product-info">
-              <p className="product-name">Basket With Handles</p>
-              <p className="product-price">$160</p>
-            </div>
-          </div>
-        </li>
-        <li className="product-list-item">
-          <div className="product">
-            <div className="product-img">
-              <img src={require('../../../assets/images/h1-product-3.jpg')} alt="h1-product-1.jpg"/>
-            </div>
-            <div className="product-info">
-              <p className="product-name">Basket With Handles</p>
-              <p className="product-price">$160</p>
-            </div>
-          </div>
-        </li>
-        <li className="product-list-item">
-          <div className="product">
-            <div className="product-img">
-              <img src={require('../../../assets/images/h1-product-4.jpg')} alt="h1-product-1.jpg"/>
-            </div>
-            <div className="product-info">
-              <p className="product-name">Basket With Handles</p>
-              <p className="product-price">$160</p>
-            </div>
-          </div>
-        </li>
-        <li className="product-list-item">
-          <div className="product">
-            <div className="product-img">
-              <img src={require('../../../assets/images/h1-product-5.jpg')} alt="h1-product-1.jpg"/>
-            </div>
-            <div className="product-info">
-              <p className="product-name">Basket With Handles</p>
-              <p className="product-price">$160</p>
-            </div>
-          </div>
-        </li>
-        <li className="product-list-item">
-          <div className="product">
-            <div className="product-img">
-              <img src={require('../../../assets/images/h1-product-6.jpg')} alt="h1-product-1.jpg"/>
-            </div>
-            <div className="product-info">
-              <p className="product-name">Basket With Handles</p>
-              <p className="product-price">$160</p>
-            </div>
-          </div>
-        </li>
-        <li className="product-list-item">
-          <div className="product">
-            <div className="product-img">
-              <img src={require('../../../assets/images/h1-product-7.jpg')} alt="h1-product-1.jpg"/>
-            </div>
-            <div className="product-info">
-              <p className="product-name">Basket With Handles</p>
-              <p className="product-price">$160</p>
-            </div>
-          </div>
-        </li>
-        <li className="product-list-item">
-          <div className="product">
-            <div className="product-img">
-              <img src={require('../../../assets/images/h1-product-8.jpg')} alt="h1-product-1.jpg"/>
-            </div>
-            <div className="product-info">
-              <p className="product-name">Basket With Handles</p>
-              <p className="product-price">$160</p>
-            </div>
-          </div>
-        </li>
+        {list}
       </ul>
     </div>
-  );
+  )
 }
 
 export default ProductList;
