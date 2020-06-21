48e8f3abf75dd2b8088ab2e18ccb59926a341f27
dieplect <diep.le@supremetech.vn>
2020-06-21 23:44:05 +0700

    Check list product

--- src/app/components/features/product-list.tsx
+++ src/app/components/features/product-list.tsx
@@ -2,20 +2,24 @@ import React from 'react';
 import ProductItem from './product-item';
 
 function ProductList(props: any) {
-  console.log('list', props, props.products);
-  const products = props.products
-  let list = products.map((item: any) => 
-    <li className="product-list-item" key={item.id.toString()}>
-      <ProductItem name={item.name}
-                    price={item.price}
-                    image={item.image} />
-    </li>
+  const products = props.products;
+  let result = products.length === 0 ? (
+    <h2 className="pb-15"> No product available!!!!!</h2>
+  ) : (
+    <ul className="product-list-inner">
+      {products.map((item: any) => 
+        <li className="product-list-item" key={item.id.toString()}>
+          <ProductItem  name={item.name}
+                        price={item.price}
+                        image={item.image} />
+        </li>
+      )}
+    </ul>
   );
+
   return (
     <div className="product-list container">
-      <ul className="product-list-inner">
-        {list}
-      </ul>
+      {result}
     </div>
   )
 }
