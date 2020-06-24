37dfa8ab52cfd4931e74d7bdafeae8303b91de65
dieplect <diep.le@supremetech.vn>
2020-06-24 15:17:01 +0700

    Add cart button

--- src/app/components/features/product-item.tsx
+++ src/app/components/features/product-item.tsx
@@ -3,10 +3,15 @@ import React from 'react';
 function ProductItem(props: any) {
   return (
     <div className="product">
-      <div className="product-img">
-        <img src={require(`../../../assets/images/${props.image}`)} alt={props.image}/>
+      <div className="product-inner">
+        <div className="product-img">
+          <img src={require(`../../../assets/images/${props.image}`)} alt={props.image}/>
+        </div>
+        <div className="product-action">
+          <button className="btn btn-primary">ADD TO CART</button>
+        </div>
       </div>
-      <div className="product-info">
+      <div className="product-text-wrapper">
         <p className="product-name">{props.name}</p>
         <p className="product-price">${props.price}</p>
       </div>
